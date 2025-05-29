7d34750414119e99c6bbfb322e1d7376998c38c2
Karlos <onekorama@gmail.com>
2025-05-29 14:24:55 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,14 @@
-#!/bin/sh
-[ -n "$PUID" ] && su-exec "$PUID":"$PGID" true
+#!/bin/bash
 
-exec transmission-daemon \
+echo "[INFO] Starting Transmission as user: $(whoami)"
+
+transmission-daemon \
   --foreground \
-  --config-dir "$TRANSMISSION_HOME" \
-  --download-dir "${TRANSMISSION_DOWNLOAD_DIR:-/downloads/complete}" \
-  --incomplete-dir-enabled "${TRANSMISSION_INCOMPLETE_DIR_ENABLED:-true}" \
-  --incomplete-dir "${TRANSMISSION_INCOMPLETE_DIR:-/downloads/incomplete}" \
-  --watch-dir-enabled "${TRANSMISSION_WATCH_DIR_ENABLED:-false}" \
-  --watch-dir "${TRANSMISSION_WATCH_DIR:-/watch}" \
-  --rpc-password "${TRANSMISSION_RPC_PASSWORD:-transmission}" \
-  --web-ui "${TRANSMISSION_WEB_HOME:-/config/flood-for-transmission}"
+  --config-dir="${TRANSMISSION_HOME:-/config}" \
+  --download-dir="${TRANSMISSION_DOWNLOAD_DIR:-/downloads/complete}" \
+  --incomplete-dir-enabled="${TRANSMISSION_INCOMPLETE_DIR_ENABLED:-true}" \
+  --incomplete-dir="${TRANSMISSION_INCOMPLETE_DIR:-/downloads/incomplete}" \
+  --watch-dir-enabled="${TRANSMISSION_WATCH_DIR_ENABLED:-false}" \
+  --watch-dir="${TRANSMISSION_WATCH_DIR:-/watch}" \
+  --rpc-password="${TRANSMISSION_RPC_PASSWORD:-transmission}" \
+  --web-ui="${TRANSMISSION_WEB_HOME:-/config/flood-for-transmission}"
